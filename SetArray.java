968e7e6d270086a9312ba3a0f96094105859a4ef
Syerdna666 <92048817+Syerdna666@users.noreply.github.com>
2023-05-08 15:01:27 +0300

    Update SetArray.java

--- SetArray.java
+++ SetArray.java
@@ -3,8 +3,8 @@ import java.util.Random;
 /**
  * Создание массива со случайной длиной от 4 до 5 и заполнение случайными числами от 0 до 9
  */
-public class GenerateArray {
-	public int [] generateArray() {
+public class SetArray {
+	public int [] setArray() {
 		Random random = new Random();
 		int [] arr = new int[random.nextInt(2) + 4];
 		for (int i = 0; i < arr.length; i++) {
